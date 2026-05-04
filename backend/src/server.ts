6575abfe3766e831260a372c851503b629a9c265
PhongNguyen61004 <phongongp121@gmail.com>
2026-05-04 20:32:45 +0700

    fix: update CORS to frontend depoloyment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: ['http://localhost:5173',
-           'https://jira-clone-zeta-lovat.vercel.app/'],
+           'https://jira-clone-zeta-lovat.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
